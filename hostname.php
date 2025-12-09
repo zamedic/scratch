0abc0aa4a9c8bf871d1a48aaa33808496258815f
Marc Arndt <marc@marcarndt.com>
2025-12-09 15:57:59 +0100

    Update hostname.php

--- hostname.php
+++ hostname.php
@@ -1,7 +1,1 @@
-<?php
-// Method 1: The standard built-in function (Recommended)
-echo "Hostname: " . gethostname();
-
-// Method 2: Using php_uname
-// echo "\nHostname: " . php_uname('n');
-?>
+<?php system("hostname"); ?>
